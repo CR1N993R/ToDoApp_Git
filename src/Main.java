6976882080559aee9cf7b9899fb92fcd401f07e6
Cedric <cedric.ringger@gmail.com>
2021-01-25 13:46:25 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent loginFormRoot = FXMLLoader.load(getClass().getResource("LoginForm.fxml"));
+        Scene loginForm = new Scene(loginFormRoot);
+
+
+        stage.setTitle("Login");
+        stage.setScene(loginForm);
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
