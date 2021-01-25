3ab1e4a374159725a50cdaf5f508a3fd246c396a
Denis Dzeladini <denis.dzeladini@edu.tbz.ch>
2021-01-25 16:02:28 +0100

    Finished Backend

--- src/main/java/ch/denis/todoapp/backend/Task.java
+++ src/main/java/ch/denis/todoapp/backend/Task.java
@@ -1,4 +1,20 @@
 package ch.denis.todoapp.backend;
 
+import java.time.LocalDate;
+
 public class Task {
+    public String taskname;
+    public String taskdescription;
+    public LocalDate dueDate;
+    public int importance;
+    public String category;
+
+
+    public Task(String taskname, String taskdescription, LocalDate dueDate, int importance, String category) {
+        this.taskname = taskname;
+        this.taskdescription = taskdescription;
+        this.dueDate = dueDate;
+        this.importance = importance;
+        this.category = category;
+    }
 }
--- src/main/java/ch/denis/todoapp/backend/Tasks.java
+++ src/main/java/ch/denis/todoapp/backend/Tasks.java
@@ -1,4 +1,55 @@
 package ch.denis.todoapp.backend;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import sun.jvm.hotspot.utilities.Observable;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 public class Tasks {
+    public static ObservableList<Task> taskList = FXCollections.observableArrayList();
+
+
+    public static void addTask(Task task){
+        taskList.add(task);
+        saveTasks();
+    }
+
+    public static void removeTask(Task task){
+        taskList.remove(task);
+        saveTasks();
+    }
+
+    public static void saveTasks(){
+        try {
+            Gson gson = new Gson();
+            FileWriter fileWriter = new FileWriter("ToDoTasks.json");
+            gson.toJson(taskList, fileWriter);
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void loadTasks(){
+        try {
+            Gson gson = new Gson();
+            JsonReader jsonReader = new JsonReader(new FileReader("ToDoTasks.json"));
+            Type type = new TypeToken<ArrayList<Task>>(){}.getType();
+            ArrayList<Task> tasks = gson.fromJson(jsonReader, type);
+            taskList.addAll(tasks);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
 }
